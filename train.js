2499871923fa003391dc5b9e8f3564bfdba1830d
Phireous <xxtrebor22xx@gmail.com>
2017-06-10 21:32:51 -0700

    finished

--- train.js
+++ train.js
@@ -1,9 +1,23 @@
 // boilerplate
 // add firebase config
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDV7h28ZqCTVDM4-hZmHpMy6FrKW_L7nAk",
+  authDomain: "train-72c24.firebaseapp.com",
+  databaseURL: "https://train-72c24.firebaseio.com",
+  projectId: "train-72c24",
+  storageBucket: "train-72c24.appspot.com",
+  messagingSenderId: "887442278568"
+};
+firebase.initializeApp(config);
+
 // connect to firebase
 // get a refernce to the database via firebase
 
-$('#SOME_BUTTON_GOES_HERE').on('click', function() {
+var database = firebase.database();
+
+$('#add-train').on('click', function() {
+  event.preventDefault();
   // collect the data from the html form, create variables to hold the data
   // train name, .... etc
   var trainName = $("#train-input").val().trim();
@@ -12,8 +26,8 @@ $('#SOME_BUTTON_GOES_HERE').on('click', function() {
   var firstTrain = $("#firsttrain-input").val().trim();
   
   // when retrieving the "first train" data, make sure to parse it into a Unix timestamp
-  var firstTrainConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
-  var firstTrainUnix = moment(firstTrainConverted).format("X");
+  
+  var firstTrainUnix = moment(firstTrain, "hh:mm").format("X");
   // `push` that data into firebase (assume that the `child_added` listener updates HTML)
   database.ref().push({
     trainName: trainName,
@@ -22,26 +36,54 @@ $('#SOME_BUTTON_GOES_HERE').on('click', function() {
     firstTrain: firstTrainUnix
   });
   // alert that train was added
-  alert("Train has been added.");
+  
   // clear out our HTML form for the next input
-  $("#train-input").html("");
-  $("#destination-input").html("");
-  $("#frequency-input").html("");
-  $("#firsttrain-input").html("");
+  $("#train-input").val("");
+  $("#destination-input").val("");
+  $("#frequency-input").val("");
+  $("#firsttrain-input").val("");
+
+  alert("Train has been added.");
 });
 
 
-something.on('child_added', function(childSnapshot) {
+database.ref().on('child_added', function(childSnapshot) {
   console.log('the childSnapshot data', childSnapshot.val());
 
   // create local variables to store the data from firebase
   var name = childSnapshot.val().trainName;
   var location = childSnapshot.val().destination;
   var frequency = childSnapshot.val().frequency;
-  var first = childSnapshot.val().firstTrain;
-  var timeInMinutes = "";
-  var tArrival = "";
+  var firstTime = childSnapshot.val().firstTrain;
+  var timeInMinutes;
+  var tArrival;
   // FIRST MAKE THE table row show up with empty strings for `timeInMinutes` / `tArrival `
+  
+  
+  // THEN DO THIS MATH
+  var firstTrainConverted = moment(firstTime, "X").subtract(1, "years");
+  console.log(firstTrainConverted);
+
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+  console.log("first train time: " + moment(firstTrainConverted, "X").format("hh:mm"));
+        // compute the difference in time from 'now' and the first train, store in var
+
+  var diffTime = moment().diff(moment(firstTrainConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+        // get the remainder of time after using `mod` with the frequency, store in var
+  var tRemainder = diffTime % frequency;
+  console.log("remainder: " + tRemainder);
+        // subtract the remainder from the frequency, store in var `timeInMinutes`
+  timeInMinutes = frequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + timeInMinutes);
+        // format `timeInMinutes` ()"make pretty") and store in var `tArrival`
+  var nextTrain = moment().add(timeInMinutes, "minutes");
+  tArrival = moment(nextTrain).format("hh:mm");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+// ITS OKAY TO JUST SHOW EMPTY STRINGS for `timeInMinutes` / `tArrival`
+  // append to our table of trains, inside the `tbody`, with a new row of the train data
   $("#train-info").prepend("<tr>" + "<td> " + name +
     " </td><td> " + location + 
     " </td><td> " + frequency + 
@@ -49,13 +91,5 @@ something.on('child_added', function(childSnapshot) {
     " </td><td> " + timeInMinutes + " </td></tr> "
   );
 
-  // THEN DO THIS MATH
-        // compute the difference in time from 'now' and the first train, store in var
-        // get the remainder of time after using `mod` with the frequency, store in var
-        // subtract the remainder from the frequency, store in var `timeInMinutes`
-        // format `timeInMinutes` ()"make pretty") and store in var `tArrival`
-
-// ITS OKAY TO JUST SHOW EMPTY STRINGS for `timeInMinutes` / `tArrival`
-  // append to our table of trains, inside the `tbody`, with a new row of the train data
 
 });
